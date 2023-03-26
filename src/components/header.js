7d856d000c9d49956443781074beb0c5a14fc898
AbhiramAtluri <saiabhiram.atluri@sjsu.edu>
2023-03-25 21:04:56 -0700

    CSS Changes

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
             <div className="row header">
                 <div className="col-md-4 header-elements">
-                    {/* <span>MRFS</span> */}
+                    <img src={"Logo.png"}></img>
                 </div>
                 <div className="col-md-8 header-elements">
                     <span>Food Security And Data Sensors Dashboard</span>
